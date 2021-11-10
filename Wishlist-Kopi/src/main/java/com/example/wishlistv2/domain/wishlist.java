52a6c9e5f75c27c69f0f8b266df395ee214b8266
AlexanderNormann <82437128+AlexanderNormann@users.noreply.github.com>
2021-11-10 09:14:42 +0100

    Delete Wishlist-Kopi directory

--- Wishlist-Kopi/src/main/java/com/example/wishlistv2/domain/wishlist.java
+++
@@ -1,4 +0,0 @@
-package com.example.wishlistv2.domain;
-
-public class wishlist {
-}
--- Wishlist-Kopi/src/main/java/com/example/wishlistv2/respositories/VareRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wishlistv2.respositories;
-
-import com.example.wishlistv2.domain.model.Vare;
-import com.example.wishlistv2.domain.servives.LoginSampleException;
-
-public interface VareRepository {
- Vare tilføjVare(Vare vare) throws LoginSampleException;
-
-}
